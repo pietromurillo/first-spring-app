1c8e3ac3fad15edd0937b62f10fc29610fc5d4e0
Pietro Murillo <pietromurillo19@gmail.com>
2025-02-17 20:33:57 -0300

    add configuration and post methods

--- src/main/java/com/murillo/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/murillo/first_spring_app/controller/HelloWorldController.java
@@ -1,14 +1,29 @@
 package com.murillo.first_spring_app.controller;
 
+import com.murillo.first_spring_app.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
+
+    @Autowired
+    private HelloWorldService helloWorldService;
+
+
+
+
     @GetMapping
     public String helloWorld(){
-        return "Hello World";
+        return HelloWorldService.helloWorld("Pietro");
+    }
+
+    @PostMapping
+    public String helloWorldPost(){
+        return "Hello World Post";
     }
 }
--- src/main/java/com/murillo/first_spring_app/service/HelloWorldService.java
+++ src/main/java/com/murillo/first_spring_app/service/HelloWorldService.java
@@ -4,4 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class HelloWorldService {
+    public static String helloWorld(String name){
+        return "Hello World " + name;
+    }
 }
