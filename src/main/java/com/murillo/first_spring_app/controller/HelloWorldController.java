e270d35a67756fbd8543acce47f3988b44ef5e81
Pietro Murillo <pietromurillo19@gmail.com>
2025-02-20 19:00:41 -0300

    HTTP Methods

--- src/main/java/com/murillo/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/murillo/first_spring_app/controller/HelloWorldController.java
@@ -1,11 +1,9 @@
 package com.murillo.first_spring_app.controller;
 
+import com.murillo.first_spring_app.domain.User;
 import com.murillo.first_spring_app.service.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello-world")
@@ -22,8 +20,8 @@ public class HelloWorldController {
         return HelloWorldService.helloWorld("Pietro");
     }
 
-    @PostMapping
-    public String helloWorldPost(){
-        return "Hello World Post";
+    @PostMapping("/{id}")
+    public String helloWorldPost(@PathVariable ("id") String id, @RequestParam(value = "filter", defaultValue = "Nenhum") String filter, @RequestBody User body){
+        return "Hello World " + filter;
     }
 }
